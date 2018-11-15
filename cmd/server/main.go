71f057b39f47e194efebd118132723e447f4ce64
Håkon Legernæs <haakoleg@tutanota.com>
2018-11-15 09:47:47 +0100

    Added support for TLS, and environment variable FORCE_TLS to only allow requests over TLS

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,33 +4,58 @@ import (
 	"log"
 	"os"
 
+	"net/http"
+
 	"github.com/haakonleg/go-e2ee-chat-engine/server"
 	"golang.org/x/net/websocket"
-	"net/http"
 )
 
+var envVars = map[string]string{
+	"PORT":         "",
+	"MONGODB_URI":  "",
+	"MONGODB_NAME": "",
+	"FORCE_TLS":    ""}
+
+func checkEnvVars() {
+	for k := range envVars {
+		val := os.Getenv(k)
+		if val == "" {
+			log.Fatalf("Error: environment variable %s is not set", k)
+		}
+		envVars[k] = val
+	}
+}
+
+// Wrapper that forces every request to use TLS
+func forceTLS(server *server.Server) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("x-forwarded-proto") != "https" &&
+			r.Header.Get("x-forwarded-proto") != "wss" {
+			http.NotFound(w, r)
+			return
+		}
+
+		wsHandler := websocket.Handler(server.WebsockHandler)
+		wsHandler.ServeHTTP(w, r)
+	}
+}
+
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("Error: environment variable PORT is not set")
-	}
-	mongoURI := os.Getenv("MONGODB_URI")
-	if mongoURI == "" {
-		log.Fatal("Error: environment variable MONGODB_URI is not set")
-	}
-	dbName := os.Getenv("MONGODB_NAME")
-	if dbName == "" {
-		log.Fatal("Error: environment variable MONGODB_NAME is not set")
-	}
+	checkEnvVars()
 
 	serverConfig := server.Config{
-		DBName:   dbName,
-		MongoURL: mongoURI}
+		DBName:   envVars["MONGODB_NAME"],
+		MongoURL: envVars["MONGODB_URI"]}
 
 	server := server.CreateServer(serverConfig)
 
-	log.Printf("Listening on port: %s\n", port)
+	log.Printf("Listening on port: %s\n", envVars["PORT"])
 
-	err := http.ListenAndServe(":"+port, websocket.Handler(server.WebsockHandler))
+	if envVars["FORCE_TLS"] == "yes" {
+		http.HandleFunc("/", forceTLS(server))
+	} else {
+		http.Handle("/", websocket.Handler(server.WebsockHandler))
+	}
+	err := http.ListenAndServe(":"+envVars["PORT"], nil)
 	log.Printf("Error occured in http listener: %s\n", err)
 }
